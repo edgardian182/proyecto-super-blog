d2d93836a26f5cd40905e14186c4d26534873b07
Edgar Quintana <edgarquintana984@hotmail.com>
2016-06-23 11:46:36 -0500

    Traduce al Español (Internacionalization) en config/locales/es.yml

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,8 @@ module ProyectoSuperBlog
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # config.i18n.default_locale = :es
-    # config.i18n.enforce_available_locales = true
+    config.i18n.default_locale = :es
+    config.i18n.enforce_available_locales = true
 
   end
 end
